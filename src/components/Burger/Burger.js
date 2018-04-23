4bdc9b7ddae85bbb5b252c00eb25b462ad55d088
Zihao Wu <zihao.wu1993@gmail.com>
2018-04-23 20:37:37 -0300

    Burger App from Udemy

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+	let transformIngredients = Object.keys(props.ingredients)
+		.map(ingredientKey => {
+			return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+				return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+			});
+		}).reduce((arr, el) => {
+			return arr.concat(el)
+		}, []);
+
+	if (transformIngredients.length === 0) {
+		transformIngredients = <p>Please start adding ingredients!</p>
+	}
+
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type="bread-top" />
+			{ transformIngredients }
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+};
+
+export default burger;
